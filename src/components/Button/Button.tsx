9c75a3e41804eada14082c684d90df59c7cb0df6
Derek Smith <dcsmith.saxophone@gmail.com>
2022-04-11 19:49:57 -0700

    ts config Button component added

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,46 @@
+import styles from "./Button.module.scss";
+
+type PrettyButtonProps = {
+  danger?: boolean;
+  onClick?: React.MouseEventHandler<HTMLButtonElement> | undefined;
+  type?: "button" | "submit" | "reset" | undefined;
+  children: React.ReactNode;
+  customStyles?: React.CSSProperties;
+  disabled?: boolean;
+  hidden?: boolean;
+  id?: string;
+  loading?: boolean;
+  form?: string;
+};
+
+const Button = ({
+  danger,
+  onClick,
+  type,
+  children,
+  customStyles,
+  disabled,
+  hidden,
+  id,
+  loading,
+  form,
+}: PrettyButtonProps) => {
+  const appliedClass = danger ? styles.danger : styles.pretty;
+
+  return (
+    <button
+      onClick={onClick}
+      type={type}
+      style={customStyles}
+      className={appliedClass}
+      disabled={disabled || loading}
+      hidden={hidden}
+      id={id}
+      form={form}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
