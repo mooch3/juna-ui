57c721cb23f47c67c389add4fef35109bb25292a
Derek Smith <dcsmith.saxophone@gmail.com>
2022-04-11 20:07:47 -0700

    set up basic rollup config

---
+++ dist/index.d.ts
@@ -0,0 +1,17 @@
+import React from 'react';
+
+declare type PrettyButtonProps = {
+    danger?: boolean;
+    onClick?: React.MouseEventHandler<HTMLButtonElement> | undefined;
+    type?: "button" | "submit" | "reset" | undefined;
+    children: React.ReactNode;
+    customStyles?: React.CSSProperties;
+    disabled?: boolean;
+    hidden?: boolean;
+    id?: string;
+    loading?: boolean;
+    form?: string;
+};
+declare const Button: ({ danger, onClick, type, children, customStyles, disabled, hidden, id, loading, form, }: PrettyButtonProps) => JSX.Element;
+
+export { Button };
